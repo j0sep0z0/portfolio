f13cf00b87cc9e7ddc13b11715d32fdd20cdd28d
j0sep0z0 <j0sep0z0@outlook.com>
2024-10-18 21:38:00 +0200

    Refactor font imports and update metadata in layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,12 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 export default function Home() {
   return (
-    <div className="flex w-full justify-center">
-      <Avatar className=" h-40 w-40">
+    <div className="flex items-center w-full justify-center p-4">
+      <Avatar className="mr-4 h-32 w-32">
         <AvatarImage src="./j0sep0z0-gritty.jpg" alt="@shadcn" />
         <AvatarFallback>JP</AvatarFallback>
       </Avatar>
+      <h1 className="text-grey-700 text-5xl ml-4">JOSE POZO</h1>
     </div>
   )
 }
