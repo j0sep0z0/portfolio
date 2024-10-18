914b27ea11309e419920dc87d438a579726b7cce
j0sep0z0 <j0sep0z0@outlook.com>
2024-10-18 15:57:32 +0200

    Refactor devDependencies and Prettier configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
+
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 export default function Home() {
   return (
-    <div className='w-full flex justify-center'>
-      <Avatar className='w-40 h-40'>
-        <AvatarImage src='./j0sep0z0-gritty.jpg' alt='@shadcn' />
-        <AvatarFallback>CN</AvatarFallback>
+    <div className="flex w-full justify-center">
+      <Avatar className=" h-40 w-40">
+        <AvatarImage src="./j0sep0z0-gritty.jpg" alt="@shadcn" />
+        <AvatarFallback>JP</AvatarFallback>
       </Avatar>
     </div>
   )
