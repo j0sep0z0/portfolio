9239e2413d4c7c1cad5f343aa9486421cde1e1d1
j0sep0z0 <j0sep0z0@outlook.com>
2024-10-20 18:36:13 +0200

    Refactor package.json and package-lock.json to add @heroicons/react dependency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,161 @@
-import { Contact, Scale } from 'lucide-react'
+'use client'
+
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/solid'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useState } from 'react'
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 
 export default function Home() {
+  const [isImageExpanded, setIsImageExpanded] = useState(false)
+
+  const toggleImageSize = () => {
+    setIsImageExpanded(!isImageExpanded)
+  }
+
+  const resetImageSize = (e: React.MouseEvent) => {
+    if ((e.target as HTMLElement).tagName !== 'IMG' && isImageExpanded) {
+      setIsImageExpanded(false)
+    }
+  }
+
   return (
     <>
-      <div className='bg-gray-400 flex flex-wrap items-center w-full rounded justify-center p-4'>
-        <Avatar className='mx-4 h-32 w-32'>
-          <AvatarImage src='./j0sep0z0-gritty.jpg' alt='@shadcn' />
-          <AvatarFallback>JP</AvatarFallback>
-        </Avatar>
-        <div className='flex flex-col justify-center items-center mx-4'>
-          <h1 className='text-4xl text-center'>JOSE POZO</h1>
-          <hr className='border-2 w-full' />
-          <h2 className='text-2xl text-center'>FULL SATCK WEB DEVELOPER</h2>
+      <header onClick={resetImageSize}>
+        <div className='bg-gray-400 flex flex-wrap items-center w-full rounded justify-center p-4'>
+          <Avatar className='mx-4 h-32 w-32'>
+            <AvatarImage src='./j0sep0z0-gritty.jpg' alt='@shadcn' />
+            <AvatarFallback>JP</AvatarFallback>
+          </Avatar>
+          <div className='flex flex-col justify-center items-center mx-4'>
+            <h1 className='text-4xl text-center'>JOSE POZO</h1>
+            <hr className='border-2 w-full' />
+            <h1 className='text-2xl text-center'>FULL SATCK WEB DEVELOPER</h1>
+          </div>
         </div>
-      </div>
-      <div className='bg-gray-100 flex rounded justify-center flex-wrap w-full my-4'>
-        <Button
-          variant='link'
-          className='text-xl hover:scale-110 active:scale-100 transition-transform duration-150'
+        <div className='bg-gray-100 flex rounded justify-center flex-wrap w-full my-4'>
+          <Button
+            variant='link'
+            className='text-xl hover:scale-110 active:scale-100 transition-transform duration-150'
+          >
+            Experience
+          </Button>
+          <Button
+            variant='link'
+            className='text-xl hover:scale-110 active:scale-100 transition-transform duration-150'
+          >
+            Projects
+          </Button>
+          <Button
+            variant='link'
+            className='text-xl hover:scale-110 active:scale-100 transition-transform duration-150'
+          >
+            About Me
+          </Button>
+          <Button
+            variant='link'
+            className='text-xl hover:scale-110 active:scale-100 transition-transform duration-150'
+          >
+            Contact
+          </Button>
+        </div>
+      </header>
+      <main onClick={resetImageSize}>
+        <section
+          id='experience'
+          className='flex justify-center mt-16 items-center'
         >
-          Experience
-        </Button>
-        <Button
-          variant='link'
-          className='text-xl hover:scale-110 active:scale-100 transition-transform duration-150'
+          <div className='sm:max-w-[40rem]'>
+            <div className='flex flex-col justify-center items-center'>
+              <h1 className='text-xl shadow-xl text-white py-2 px-4 rounded self-start bg-gray-400 w-full'>
+                Experience
+              </h1>
+              <div className='my-4 bg-gray-200 shadow-xl rounded py-2 px-4 flex flex-col justify-center items-center'>
+                <h1 className='self-start my-2 text-xl'>Front End Developer</h1>
+                <p className='self-start text-lg font'>
+                  Igrowker - Acelerando Juniors IT
+                </p>
+                <p className='self-start my-2'>
+                  [ September 2024 – Present | Remote | Valencia, Spain ]
+                </p>
+                <p className='my-2'>
+                  Development of "GoPass," a secure platform aimed at combating
+                  the growing issue of illegal ticket resale in Argentine
+                  football. This innovative project ensures the authenticity of
+                  each ticket, providing a trustworthy solution for fans and
+                  organizers alike.
+                </p>
+                <p className='my-2 self-start'>
+                  React, TypeScript, Redux Toolkit, Axios, Tailwind CSS, Git,
+                  GitHub.
+                </p>
+                <Link
+                  href='https://github.com/igrowker/i003-gopass-front'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='text-blue-800 my-2 text-xl bg-gray-100 hover:bg-gray-400 hover:text-white rounded py-1 px-2 flex self-end items-center'
+                >
+                  GoPass repository
+                  <ArrowTopRightOnSquareIcon className='h-5 w-5 text-blue-800 ml-2' />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section
+          id='projects'
+          className='flex justify-center mt-16 items-center'
         >
-          Projects
-        </Button>
-        <Button
-          variant='link'
-          className='text-xl hover:scale-110 active:scale-100 transition-transform duration-150'
-        >
-          About Me
-        </Button>
-        <Button
-          variant='link'
-          className='text-xl hover:scale-110 active:scale-100 transition-transform duration-150'
-        >
-          Contact
-        </Button>
-      </div>
+          <div className='sm:max-w-[40rem]'>
+            <div className='flex flex-col justify-center items-center'>
+              <h1 className='text-xl shadow-xl text-white py-2 px-4 rounded self-start bg-gray-400 w-full'>
+                Projects
+              </h1>
+              <div className='my-4 bg-gray-200 shadow-xl rounded py-2 px-4 flex flex-col justify-center items-center'>
+                <Image
+                  width={1200}
+                  height={600}
+                  src='/daily-planner-views.png'
+                  alt='Daily Planner Views'
+                  className={`rounded my-2 transition-transform duration-300 ${
+                    isImageExpanded ? 'scale-150 shadow-2xl' : 'scale-100'
+                  }`}
+                  onClick={toggleImageSize}
+                />
+                <h1 className='self-start my-2 text-xl'>Daily Planner</h1>
+                <p className='self-start text-lg font'>
+                  Final Project of the Full Stack Web Development Bootcamp
+                </p>
+                <p className='self-start my-2'>
+                  [ Currently improving the app and implementing TypeScript and
+                  Redux ]
+                </p>
+                <p className='my-2'>
+                  Designed to optimize the daily management of salons and beauty
+                  centers. It allows scheduling appointments, managing clients
+                  and services, and offers a complete history of the activities
+                  performed.
+                </p>
+                <Link
+                  href='https://daily-planner-site.netlify.app/'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='text-blue-800 my-2 text-xl bg-gray-100 hover:bg-gray-400 hover:text-white rounded py-1 px-2 flex self-end items-center'
+                >
+                  Daily Planner
+                  <ArrowTopRightOnSquareIcon className='h-5 w-5 text-blue-800 ml-2' />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer
+        onClick={resetImageSize}
+        className='bg-gray-400 w-full rounded h-24 mt-16 p-4'
+      ></footer>
     </>
   )
 }
