99ff140d1fdd2e6d251613533fd475ff40d1b34b
j0sep0z0 <j0sep0z0@outlook.com>
2025-01-28 00:12:53 +0100

    Update global styles and modify layout for improved typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en' className='scroll-smooth'>
+    <html lang='en'  className='scroll-smooth'>
       <body
         className={cn(
           poppins.className,
